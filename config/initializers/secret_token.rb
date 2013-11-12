aabf4d0471f0e7bd60f3c84656991548946fb9a5
Rachel Loziuk <rachel.loziuk@gmail.com>
2013-11-12 17:11:53 -0600

    intial commit; installed foundation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rach7110GithubIo::Application.config.secret_token = '006494b7519aff7c70a388f0e30b3abad02d031b6489cf47548730075b6c80405fad9378f9cdfe87b9f9bdfbb28e61c0b5cbd73341f1cb1eec909b987d3ea1da'
